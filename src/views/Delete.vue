f05847176936b894486c86bb7c901830f2933f11
Ahmad Habibi <109415784+ahmdxhabibi@users.noreply.github.com>
2022-09-11 20:54:46 +0700

    Add files via upload

---
+++ src/views/Delete.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p
+      class="text-light p-3 bg-danger bg-opacity-10 border border-info border-start-0 rounded-end"
+    >
+      Are you sure you delete this ?
+    </p>
+    <button class="btn btn-danger" @click="deleteUser()">Delete</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  methods: {
+    async deleteUser() {
+      let a = window.confirm("You want to delete the user?");
+      if (a) {
+        const user = await axios.delete("https://reqres.in/api/users/2");
+        console.log(user);
+        alert("User deleted!");
+      }
+    },
+  },
+};
+</script>
---
+++ src/views/Edit.vue
@@ -0,0 +1,42 @@
+<template>
+  <form>
+    <div class="user-update">
+      <h2>Update user</h2>
+      <div class="mb-3">
+        <label class="form-label">Name</label>
+        <input class="form-control" type="text" v-model="name" />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Job</label>
+        <input class="form-control" type="text" v-model="job" />
+      </div>
+      <button class="btn btn-primary" @click="updateUser">Update</button>
+    </div>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      name: "",
+      job: "",
+    };
+  },
+  methods: {
+    async updateUser() {
+      try {
+        const user = await axios.put("https://reqres.in/api/users/2", {
+          name: this.name,
+          job: this.job,
+        });
+        console.log(user);
+        alert("User updated!");
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+};
+</script>
